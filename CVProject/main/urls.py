7526debe54bce379c421a8938540fb1b47787385
Nazar Chaban <134311145+NazarChaban@users.noreply.github.com>
2025-05-19 23:43:39 +0300

    Detail Page View

--- CVProject/main/urls.py
+++ CVProject/main/urls.py
@@ -5,4 +5,5 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.cv_list_view, name='cv_list'),
+    path('cv/<int:cv_id>/', views.cv_detail_view, name='cv_detail'),
 ]
--- CVProject/main/views.py
+++ CVProject/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import CV
 
 
@@ -8,3 +8,14 @@ def cv_list_view(request):
         'cvs': cvs
     }
     return render(request, 'main/cv_list.html', context)
+
+
+def cv_detail_view(request, cv_id):
+    cv = get_object_or_404(
+        CV.objects.prefetch_related('skills', 'projects'),
+        pk=cv_id
+    )
+    context = {
+        'cv': cv
+    }
+    return render(request, 'main/cv_detail.html', context)
