dadbc6cb5409436444ace71616b56a457f1f5242
Nazar Chaban <134311145+NazarChaban@users.noreply.github.com>
2025-05-23 12:50:41 +0300

    Better route order

--- CVProject/CVProject/urls.py
+++ CVProject/CVProject/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
     path('audit/', include('audit.urls')),
+    path('', include('main.urls')),
 ]
